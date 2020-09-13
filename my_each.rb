3e4f9a3c11bda92387487315e7cde3f363881f58
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-13 17:13:03 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,10 @@
 def my_each(array) # put argument(s) here
   # code here
-  i = 0
-  while i < array.length
-    yeild array[i]
-    i = i + 1
-  end
-  array
+  if block_given?
+    i = 0
+    while i < array.length
+      yeild array[i]
+      i = i + 1
+    end
+    array
 end
